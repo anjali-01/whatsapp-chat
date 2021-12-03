f61489319956197f353e183165a403a94a7a8664
anjali-01 <anjalidaivapu@gmail.com>
2021-12-03 22:02:12 +0530

    added source code

---
+++ pages/nav.js
@@ -0,0 +1,9 @@
+import React from "react";
+ export default function Nav(){
+     return(
+         <div className="nav">
+         <p className="head">Chat Anonymous<img width="40px"src="https://img.icons8.com/external-tal-revivo-filled-tal-revivo/48/000000/external-anonymous-user-with-hat-and-glasses-layout-security-filled-tal-revivo.png"/></p>
+          <a href="https://github.com/anjali-01"><img src="https://img.icons8.com/clouds/70/000000/github.png"/></a>
+         </div>
+         )
+ }
