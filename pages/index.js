1261bb98e8ef64e70687dd40bcbef9f3a57d69c0
anjali-01 <anjalidaivapu@gmail.com>
2021-12-03 22:20:14 +0530

    changed url

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,8 @@ export default function Index(){
      onChange={(e)=>updateUser(e.target.value)}/><br/>
     </div>
      <div className="btn">
-    <a className="tag" href={`wa.me/91${ph}`}>Chat in Whatsapp<img width="30px" src="https://img.icons8.com/material-rounded/50/000000/whatsapp.png"/></a>
-    <a className="tag" href={`t.me/${user}`}>Chat in Telegram<img width="30px" src="https://img.icons8.com/material-sharp/24/000000/telegram-app.png"/></a>
+    <a className="tag" href={`https://api.whatsapp.com/send/?phone=${ph}&text&app_absent=0`}>Chat in Whatsapp<img width="30px" src="https://img.icons8.com/material-rounded/50/000000/whatsapp.png"/></a>
+    <a className="tag" href={`https://t.me/${user}`}>Chat in Telegram<img width="30px" src="https://img.icons8.com/material-sharp/24/000000/telegram-app.png"/></a>
     </div>
     </div>
   )
